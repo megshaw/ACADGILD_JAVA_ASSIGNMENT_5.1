8a2cf85dee83c73ea9af07c133dd8cfac87571d4
meghana <meghana@Namsystech-Dev-01>
2017-03-24 15:58:32 +0530

    Assignment 5.1

---
+++ JavaAssignment5.1/src/Shapes/Rectangle.java
@@ -0,0 +1,27 @@
+
+package Shapes;
+
+import Shapes.Shape;
+
+public class Rectangle extends Shape {
+    private final double width, length; //sides
+
+    public Rectangle() {
+        this(1,1);
+    }
+    public Rectangle(double width, double length) {
+        this.width = width;
+        this.length = length;
+    }
+
+    public double area() {
+        // A = w * l
+        return width * length;
+    }
+
+    public double perimeter() {
+        // P = 2(w + l)
+        return 2 * (width + length);
+    }
+
+}
